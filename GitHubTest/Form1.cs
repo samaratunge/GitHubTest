281f37247197802e26b1a7e97d7a929159e82796
samaratunge <samaratunge@gmail.com>
2025-08-06 10:02:17 +1200

    Add new feature message box

--- GitHubTest/Form1.cs
+++ GitHubTest/Form1.cs
@@ -19,7 +19,7 @@ namespace GitHubTest
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hi Udara How are you");
+            MessageBox.Show("Hi Udara How are you? ");
         }
     }
 }
